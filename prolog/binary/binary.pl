814ef75058fa75df88833f7bc2225eae56c015ae
Chris Guidry <chris@theguidrys.us>
2023-01-16 15:34:25 -0500

    Binary string.

---
+++ prolog/binary/binary.pl
@@ -0,0 +1,14 @@
+binary(String, Decimal) :-
+    % Convert the string into individual characters, reversed to LittleEndian
+    string_chars(String, BigEndian),
+    reverse(BigEndian, LittleEndian),
+    % Convert the characters to integers, ensuring they are either 0 or 1
+    maplist(atom_number, LittleEndian, Digits),
+    forall(member(D, Digits), between(0, 1, D)),
+    % Produce the places for each slot in the list (this is like Python's enumerate)
+    length(Digits, Length),
+    EndIndex is Length - 1,
+    setof(X, between(0, EndIndex, X), Places),
+    % Do the multiplication of each place, then sum it to get the final answer
+    maplist([Place, Digit, Y] >> (Y is Digit * 2 ** Place), Places, Digits, Computed),
+    sum_list(Computed, Decimal).
