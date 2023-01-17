a24a5dcc2350965b6abc143eaa413aa6c3d47504
Chris Guidry <chris@theguidrys.us>
2023-01-17 13:13:47 -0500

    Refactoring Prolog anagram.

--- prolog/anagram/anagram.pl
+++ prolog/anagram/anagram.pl
@@ -1,16 +1,16 @@
-is_anagram(Word, Option) :-
+letters(Word, Letters) :-
+    string_chars(Word, Chars),
+    msort(Chars, Letters).
+
+anagram(Word, Option) :-
     % anagrams are case insensitive
     string_lower(Word, LowerWord),
     string_lower(Option, LowerOption),
     % A word is not its own anagram
     LowerWord \= LowerOption,
     % Compare the set of letters together
-    string_chars(LowerWord, WordChars),
-    msort(WordChars, SortedWordChars),
-    string_chars(LowerOption, OptionChars),
-    msort(OptionChars, SortedOptionChars),
-    SortedWordChars == SortedOptionChars.
-
+    letters(LowerWord, Letters),
+    letters(LowerOption, Letters).
 
 anagram(Word, Options, Matching) :-
-    include([Option] >> (is_anagram(Word, Option)), Options, Matching).
+    include(anagram(Word), Options, Matching).
