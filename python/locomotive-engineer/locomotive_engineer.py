3eef064df89711c500e85c1b0714b1722ffd7334
Chris Guidry <chris@theguidrys.us>
2023-01-29 12:20:02 -0500

    Four more Python exercises.

---
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -0,0 +1,63 @@
+"""Functions which helps the locomotive engineer to keep track of the train."""
+
+
+def get_list_of_wagons(*args):
+    """Return a list of wagons.
+
+    :param: arbitrary number of wagons.
+    :return: list - list of wagons.
+    """
+    return list(args)
+
+
+# TODO: define the 'fixListOfWagons()' function
+def fix_list_of_wagons(each_wagons_id, missing_wagons):
+    """Fix the list of wagons.
+
+    :parm each_wagons_id: list - the list of wagons.
+    :parm missing_wagons: list - the list of missing wagons.
+    :return: list - list of wagons.
+    """
+    locomotive_position = each_wagons_id.index(1)
+    return (
+        [1]
+        + missing_wagons
+        + each_wagons_id[locomotive_position + 1 :]
+        + each_wagons_id[:locomotive_position]
+    )
+
+
+# TODO: define the 'add_missing_stops()' function
+def add_missing_stops(route, **kwargs):
+    """Add missing stops to route dict.
+
+    :param route: dict - the dict of routing information.
+    :param: arbitrary number of stops.
+    :return: dict - updated route dictionary.
+    """
+    return {**route, "stops": [stop for key, stop in sorted(kwargs.items())]}
+
+
+# TODO: define the 'extend_route_information()' function
+def extend_route_information(route, more_route_information):
+    """Extend route information with more_route_information.
+
+    :param route: dict - the route information.
+    :param more_route_information: dict -  extra route information.
+    :return: dict - extended route information.
+    """
+    return {**route, **more_route_information}
+
+
+# TODO: define the 'fix_wagon_depot()' function
+def fix_wagon_depot(wagons_rows):
+    """Fix the list of rows of wagons.
+
+    :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
+    :return: list[list[tuple]] - list of rows of wagons.
+    """
+    transposed = [[None] * 3 for _ in range(3)]
+    for i in range(len(wagons_rows)):
+        for j in range(len(wagons_rows)):
+            transposed[i][j] = wagons_rows[j][i]
+    return transposed
